9df9730c414059c209ac586ed86aa49e7700d33d
agustisanchez <agusti.sanchez@gmail.com>
2013-05-27 20:56:20 +0200

    simplified

--- mvn-int.sh
+++ mvn-int.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
-mvn -Pintegration -DgeogrepConfigurationTarget=devtest -DindexerSearchTarget=devtest verify > mvn.out 2>&1
+mvn -Pintegration -DgeogrepConfigurationTarget=devtest verify > mvn.out 2>&1
 
