b94e5af321c8d5b41f6358a437bd5a95d53f9ac1
agustisanchez <agusti.sanchez@gmail.com>
2013-09-28 14:20:42 +0200

    log file

--- mvn-int-loc.sh
+++ mvn-int-loc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
-mvn -Pintegration -DgeogrepConfigurationTarget=devtest -am -pl geo-rest verify > mvn.out 2>&1
+mvn -Pintegration -Dlogback.configurationFile="$LOGBACK_FILE" -DgeogrepConfigurationTarget=devtest -am -pl geo-rest verify > mvn.out 2>&1
