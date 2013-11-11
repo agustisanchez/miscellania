cc77216a333832f275c8a1707aab06b0a0e43695
agustisanchez <agusti.sanchez@gmail.com>
2013-11-11 21:33:06 +0100

    profiles

--- mvn-int-iam.sh
+++ mvn-int-iam.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
-#mvn -Pintegration -Dldap=on -DgeogrepConfigurationTarget=devtest -Dldap.testTenantId=patata -am -pl gp-iam-rest verify > mvn.out 2>&1
-mvn -Pintegration -Dlogback.configurationFile="$LOGBACK_FILE" -Dldap=on -DgeogrepConfigurationTarget=devtest -am -pl iam-rest verify > mvn.out 2>&1
+mvn -Dlogback.configurationFile="$LOGBACK_FILE" -Dspring.profiles.active=elasticsearch -DgeogrepConfigurationTarget=devtest -am -pl iam-rest verify > mvn.out 2>&1
