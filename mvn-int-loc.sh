7ff4227424ee0fbdaaeeb4f61b5f091f87ad7e30
agustisanchez <agusti.sanchez@gmail.com>
2013-11-17 09:35:03 +0100

    typo

--- mvn-int-loc.sh
+++ mvn-int-loc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
-mvn -Dlogback.configurationFile="$LOGBACK_FILE" -DgeogrepConfigurationTarget=devtest -Dspring.profiles.active+elasticsearch -am -pl geo-rest verify > mvn.out 2>&1
+mvn -Dlogback.configurationFile="$LOGBACK_FILE" -DgeogrepConfigurationTarget=devtest -Dspring.profiles.active=elasticsearch -am -pl geo-rest verify > mvn.out 2>&1
