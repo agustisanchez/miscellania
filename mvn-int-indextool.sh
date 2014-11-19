ba9c327a39b067c475ccdcec34cdb84b5db6eb65
agustisanchez <agusti.sanchez@gmail.com>
2014-11-19 22:14:27 +0100

    no more integration profile

--- mvn-int-indextool.sh
+++ mvn-int-indextool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
-mvn -Pintegration -Dlogback.configurationFile="$LOGBACK_FILE" -DgeogrepConfigurationTarget=devtest -am -pl indextool-war verify > mvn.out 2>&1
+mvn -Dlogback.configurationFile="$LOGBACK_FILE" -DgeogrepConfigurationTarget=devtest -am -pl indextool-war verify > mvn.out 2>&1
