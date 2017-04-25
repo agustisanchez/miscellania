1270c369f3546f2b10b2ed21d0eb7e06c15b9d95
Agustí Sánchez <agusti.sanchez@gmail.com>
2017-04-26 00:50:50 +0200

    Create export-from-git.sh

---
+++ export-from-git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Exports from git into a temp directory
+# and creates a ZIP archive.
+set -e
+trap clean EXIT
+function clean {
+   if [ -n "$TMPDIR" ]
+   then
+      rm -fR $TMPDIR
+   fi
+}
+tag=$1
+if [ -z "$tag" ]
+then
+        read -p "Tag: " tag
+fi
+TMPDIR=`mktemp -d`
+nom=${PWD}/pam-portal-src-${tag}.zip
+if [ -e "$nom" ]
+then
+        rm -v $nom
+fi
+git clone --depth 1 --branch $tag git@gitlab.com:sabart/Project1B.git $TMPDIR( cd $TMPDIR; zip -qr ${nom} . -x .\*  )
+echo "${nom}"
