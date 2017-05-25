ff97ead8a0c17d1e59a2990c9f8fcfc5c31404a0
Agustí Sánchez <agusti.sanchez@gmail.com>
2017-05-25 03:34:21 +0200

    Update export-from-git.sh

--- export-from-git.sh
+++ export-from-git.sh
@@ -9,17 +9,26 @@ function clean {
       rm -fR $TMPDIR
    fi
 }
-tag=$1
-if [ -z "$tag" ]
+# tag local_name remote_name
+if [ "$#" -ge "3" ]
 then
-        read -p "Tag: " tag
+    tag=$1
+    local_name=$2
+    remote_name=$3
+else
+    read -p "Tag: " tag
+    read -p "Local name: " local_name
+    read -p "Remote name: " remote_name
 fi
 TMPDIR=`mktemp -d`
-nom=${PWD}/pam-portal-src-${tag}.zip
-if [ -e "$nom" ]
+base_name=${local_name}-src-${tag}
+ZIPDIR=$TMPDIR/${base_name}
+mkdir $ZIPDIR
+zip_name=${PWD}/${base_name}.zip
+if [ -e "${zip_name}" ]
 then
-        rm -v $nom
+        rm -v ${zip_name}
 fi
-git clone --depth 1 --branch $tag git@gitlab.com:<user>/<project>.git $TMPDIR
-( cd $TMPDIR; zip -qr ${nom} . -x .\*  )
-echo "${nom}"
+git clone --depth 1 --branch $tag git@gitlab.com:sabart/${remote_name}.git $ZIPDIR
+( cd $TMPDIR; zip -qr ${zip_name} . -x ${base_name}/.\*  )
+echo "${zip_name}"
